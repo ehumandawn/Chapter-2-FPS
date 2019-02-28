593668750257e720b413bfc3efc972a34065ddb0
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-02-27 17:39:02 -0800

    Dying now when hit

--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -18,5 +18,17 @@ public class PlayerCharacter : MonoBehaviour {
 	public void Hurt (int damage) {
 		_health -= damage;
 		Debug.Log ("Health: " + _health);
+		if (_health <= 0) {
+			StartCoroutine (Die ());
+		}
 	}
+
+	private IEnumerator Die() {
+		this.transform.Rotate (-75, 0, 0);
+
+		yield return new WaitForSeconds (1.5f);
+
+		Destroy (this.gameObject);
+	}
+
 }
